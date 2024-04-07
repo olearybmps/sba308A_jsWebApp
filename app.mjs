86e721527088e7ca99496ed77df7e7cf6c8b2533
olearybm <90865347+olearybm@users.noreply.github.com>
2024-04-07 12:59:02 -0400

    Added helper.mjs, random quote and greeting

--- app.mjs
+++ app.mjs
@@ -1,13 +1,28 @@
 // app.mjs
-import { getRandomQuote } from './quotable_api.mjs';
+import { getRandomQuote } from "./quotable_api.mjs";
+import { renderQuote, getCurrentDateTime } from "./helper.mjs";
 
-async function testGetRandomQuote() {
+function displayDayDateTime() {
+    const greetingElement = document.getElementById('greetingDate');
+    greetingElement.innerHTML = "";
+    greetingElement.innerHTML = getCurrentDateTime();
+}
+
+async function displayRandomQuote() {
     try {
-        const randomQuote = await getRandomQuote();
-        console.log("Random Quote:", randomQuote);
+        const quote = await getRandomQuote();
+        console.log("Quote:", quote);
+
+        const quoteElement = renderQuote(quote);
+        const containerElement = document.getElementById("quoteContainer");
+        containerElement.innerHTML = "";
+        containerElement.appendChild(quoteElement);
     } catch (error) {
-        console.error("Error in getRandomQuote:", error);
+        console.error("Error fetching quote:", error);
     }
 }
 
-testGetRandomQuote();
+displayDayDateTime();
+displayRandomQuote();
+
+
---
+++ helper.mjs
@@ -0,0 +1,49 @@
+// helper.mjs
+export function renderQuote(quote) {
+    const quoteElement = document.createElement("div");
+    quoteElement.classList.add("quote");
+    quoteElement.textContent = `"${quote.content}" - ${quote.author}`;
+    return quoteElement;
+}
+
+// https://stackoverflow.com/questions/3552461/how-do-i-format-a-date-in-javascript
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString
+export function getCurrentDateTime() {
+    const today = new Date();
+
+    const options = {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+    };
+    const strTime = today.toLocaleTimeString("en-US", options);
+    const formatTime = strTime.replace(":00", "");
+
+    const dateOptions = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+    const strDate = today.toLocaleDateString("en-US", dateOptions);
+
+    const greeting = getGreeting(today.getHours());
+
+    const greetDateTimeMsg = `
+                                <div class="time">${formatTime}</div>
+                                <div class="date">${strDate.toUpperCase()}</div>
+                                <div class="greeting">${greeting}</div>
+                            `;
+
+    return greetDateTimeMsg;
+}
+
+function getGreeting(hour) {
+    if (hour >= 5 && hour < 12) {
+        return "Good morning";
+    } else if (hour >= 12 && hour < 18) {
+        return "Good afternoon";
+    } else {
+        return "Good evening";
+    }
+}
